779243c2d7969068641bf9db03d7226a6964335c
acmaser12 <49576923+acmaser12@users.noreply.github.com>
2020-11-10 20:22:39 -0600

    Implemented basic decimal calculation and UI

--- lib/main.dart
+++ lib/main.dart
@@ -1,117 +1,446 @@
+// Import MaterialApp and other widgets which we can use to quickly create a material app
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(MyApp());
-}
+// Code written in Dart starts exectuting from the main function. runApp is part of
+// Flutter, and requires the component which will be our app's container. In Flutter,
+// every component is known as a "widget".
+void main() => runApp(new ConvertItApp());
 
-class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
+// Every component in Flutter is a widget, even the whole app itself
+class ConvertItApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
+    return new MaterialApp(
+      title: 'Convert It',
+      home: new TodoList(),
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-        // This makes the visual density adapt to the platform that you run
-        // the app on. For desktop platforms, the controls will be smaller and
-        // closer together (more dense) than on mobile platforms.
-        visualDensity: VisualDensity.adaptivePlatformDensity,
-      ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
+        primaryColor: Colors.white,
+        textTheme: TextTheme(
+          bodyText2: TextStyle(
+            color: Colors.black
+          ), 
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
+  }
+}
+
+class TodoList extends StatefulWidget {
+  @override
+  createState() => TodoListState();
+}
+
+class TodoListState extends State<TodoList> {
+  final GlobalKey<ScaffoldState> _scaffoldKey = new GlobalKey<ScaffoldState>();
+  String baselineArchVal = "";
+  String baselineCivVal = "";
+  String baseline = "";
+  bool isArchDecimal = true;
+  String knownArchVal = "";
+  String knownCivVal = "";
+
+
+  Widget _buildBody() {
+    return new Scaffold(
+        body: DefaultTextStyle(
+          style: TextStyle(
+            color: Colors.black,
+            fontFamily: 'Roboto',
+            ),
+          child: SingleChildScrollView(
+            child: GestureDetector (
+              onTap: () {
+                FocusScope.of(context).requestFocus(new FocusNode());
+              },
+              child: Container(
+                child: Column (
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.all(10.0),
+                      child: _buildBaselineContainer()
+                    ),
+                    Padding(
+                      padding: new EdgeInsets.all(10.0),
+                      child: _buildKnownValueContainer())
+                  ],),
+                padding: new EdgeInsets.all(10.0),
+                color: Colors.transparent,
+              ),
+            ),
+          )
+        ),
+        backgroundColor: Colors.grey[300],
+    );
+  }
+
+Widget _buildBaselineContainer() {
+    return new Container (
+      decoration: new BoxDecoration(
+        color: Colors.white,
+        boxShadow: [
+          BoxShadow(
+            color: Colors.grey.withOpacity(0.5),
+            spreadRadius: 3,
+            blurRadius: 3,
+            offset: Offset(0, 3), // changes position of shadow
+          ),
+        ],
+      ),
+      child: Padding(
+        padding: new EdgeInsets.all(20.0),
+        child: Column (
+          children: [
+            Row(
+              children: [
+                Text(
+                  'Baseline',
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 20
+                  ),
+                ),
+                Switch(
+                  value: isArchDecimal, 
+                  onChanged: (value) => {
+                  }
+                  ),
+                // add toggle for ft/in - decimal here
+              ]),
+            Padding(
+              padding: new EdgeInsets.fromLTRB(0, 20, 0, 15),
+              child: Row(
+                children: [
+                  Text(
+                    'Architecural:',
+                    style: TextStyle(
+                      fontSize: 15
+                    ),
+                  ),
+              ]),
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.end,
+              crossAxisAlignment: CrossAxisAlignment.end,
+              children: [
+                Expanded(
+                  child: TextField(
+                    onChanged: (val) {
+                       _setBaselineArchValue(val);
+                    },
+                    decoration: InputDecoration(
+                      border: OutlineInputBorder(),
+                      fillColor: Colors.white,
+                      filled: true,
+                    ),
+                    keyboardType: TextInputType.numberWithOptions(
+                      signed: false, decimal: true,
+                    ),
+                  ),
+                )
+              ],
+            ),
+            Padding(
+              padding: new EdgeInsets.fromLTRB(0, 20, 0, 15),
+              child: Row(
+                children: [
+                  Text(
+                    'Civil:',
+                    style: TextStyle(
+                      fontSize: 15
+                    ),
+                  ),
+              ]),
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.end,
+              crossAxisAlignment: CrossAxisAlignment.end,
+              children: [
+                Expanded(
+                  child: TextField(
+                    onChanged: (val) {
+                       _setBaselineCivValue(val);
+                    },
+                    decoration: InputDecoration(
+                      border: OutlineInputBorder(),
+                      fillColor: Colors.white,
+                      filled: true,
+                    ),
+                    keyboardType: TextInputType.numberWithOptions(
+                      signed: false, decimal: true
+                    ),
+                  ),
+                )
+              ],
+            ),
+          ]),
+        )
+    );
+  }
+
+  Widget _getBaseArchInput(bool val) {
+    if (val) {
+      return new Row(
+        mainAxisAlignment: MainAxisAlignment.end,
+        crossAxisAlignment: CrossAxisAlignment.end,
+        children: [
+          Expanded(
+            child: TextField(
+              onChanged: (val) {
+                  _setBaselineArchValue(val);
+              },
+              decoration: InputDecoration(
+                border: OutlineInputBorder(),
+                fillColor: Colors.white,
+                filled: true,
+              ),
+              keyboardType: TextInputType.numberWithOptions(
+                signed: false, decimal: true,
+              ),
+            ),
+          )
+        ],
+      );
+    } else {
+      return new Row(
+
+      );
+    }
+  }
+
+  Widget _buildKnownValueContainer() {
+    return new Container (
+      decoration: new BoxDecoration(
+        color: Colors.white,
+        boxShadow: [
+          BoxShadow(
+            color: Colors.grey.withOpacity(0.5),
+            spreadRadius: 3,
+            blurRadius: 3,
+            offset: Offset(0, 3), // changes position of shadow
+          ),
+        ],
+      ),
+      child: Padding(
+        padding: new EdgeInsets.all(20.0),
+        child: Column (
+          children: [
+            Row(
+              children: [
+                Text(
+                  'Known Values (enter one)',
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 20
+                  ),
+                ),
+              ]),
+            Padding(
+              padding: new EdgeInsets.fromLTRB(0, 20, 0, 15),
+              child: Row(
+                children: [
+                  Text(
+                    'Known Architecural Value:',
+                    style: TextStyle(
+                      fontSize: 15
+                    ),
+                  ),
+              ]),
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.end,
+              crossAxisAlignment: CrossAxisAlignment.end,
+              children: [
+                Expanded(
+                  child: TextField(
+                    onChanged: (val) {
+                       _setKnownArchValue(val);
+                    },
+                    decoration: InputDecoration(
+                      border: OutlineInputBorder(),
+                      fillColor: Colors.white,
+                      filled: true,
+                    ),
+                    keyboardType: TextInputType.numberWithOptions(
+                      signed: false, decimal: true
+                    ),
+                  ),
+                )
+              ],
+            ),
+            Padding(
+              padding: new EdgeInsets.fromLTRB(0, 20, 0, 15),
+              child: Row(
+                children: [
+                  Text(
+                    'Known Civil Value:',
+                    style: TextStyle(
+                      fontSize: 15
+                    ),
+                  ),
+              ]),
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.end,
+              crossAxisAlignment: CrossAxisAlignment.end,
+              children: [
+                Expanded(
+                  child: TextField(
+                    onChanged: (val) {
+                       _setKnownCivValue(val);
+                    },
+                    decoration: InputDecoration(
+                      border: OutlineInputBorder(),
+                      fillColor: Colors.white,
+                      filled: true,
+                    ),
+                    keyboardType: TextInputType.numberWithOptions(
+                      signed: false, decimal: true
+                    ),
+                  ),
+                )
+              ],
+            ),
+          ]),
+        )
+    );
+  }
+
+  // set baselineArchValue
+  void _setBaselineArchValue(String val) {
+    // call setState to reset UI and update stored value
+    setState(() => baselineArchVal = val);
+  }
+
+    // set baselineCivValue
+  void _setBaselineCivValue(String val) {
+    // call setState to reset UI and update stored value
+    setState(() => baselineCivVal = val);
+  }
+
+    // set knownArchValue
+  void _setKnownArchValue(String val) {
+    // call setState to reset UI and update stored value
+    setState(() => knownArchVal = val);
+  }
+
+    // set knownCivValue
+  void _setKnownCivValue(String val) {
+    // call setState to reset UI and update stored value
+    setState(() => knownCivVal = val);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      key: _scaffoldKey,
+      appBar: new AppBar(
+        title: new Text('Convert It'),
+        backgroundColor: Colors.white,
+        
+      ),
+      body: _buildBody(),
+      floatingActionButton: new FloatingActionButton.extended(
+        backgroundColor: Colors.yellow[800],
+        shape: BeveledRectangleBorder(),
+        onPressed: () {
+          // DEBUG
+          //print("baseline arch: " + baselineArchVal),
+          //print("baseline civ " + baselineCivVal),
+          //print("known arch: " + knownArchVal),
+          //print("known civ: " + knownCivVal)
+          if (_checkIfReadyToConvert()) {
+            _pushAddResultsScreen();
+          } else {
+            final snackBar = SnackBar(
+              content: Text('Please enter a value in only one Known input.')
+            );
+           _scaffoldKey.currentState.showSnackBar(snackBar);
+          }
+        }, 
+        tooltip: 'Convert it',
+        icon: Icon(Icons.calculate),
+        label: Text("Convert-It"),
+      ),
+    );
+  }
+
+  void _pushAddResultsScreen() {
+    // Push this page onto the stack
+    Navigator.of(context).push(
+      // MaterialPageRoute will automatically animate the screen entry, as well
+      // as adding a back button to close it
+      new MaterialPageRoute(
+        builder: (context) {
+          return new Scaffold(
+            appBar: new AppBar(
+              title: new Text('Conversion Results')
+            ),
+            body: _buildResultsPage(),
+          );
+        }
+      )
+    );
+  }
+
+  Widget _buildResultsPage() {
+    return new Container(
+      child: Column(
+        children: [
+          Container (
+            decoration: new BoxDecoration(
+              color: Colors.white,
+              boxShadow: [
+                BoxShadow(
+                  color: Colors.grey.withOpacity(0.5),
+                  spreadRadius: 3,
+                  blurRadius: 3,
+                  offset: Offset(0, 3), // changes position of shadow
+                ),
+              ],
+            ),
+            child: Column(
+              children: [
+                Text("Baseline: "),
+                Text(_getBaseline()),
+              ],
+            ),
+          ),
+          Text("Conversion Output: "),
+          Text(_getConversionValue())
+        ],
+      ),
+    );
+  }
+
+  String _getConversionValue() {
+    if (knownArchVal == "") {
+      // this means we are using civil to convert the architectural value
+      double civil = double.parse(knownCivVal);
+      String arch = (civil - double.parse(baseline)).toString();
+      return "Architectural Converted Value: " + arch;
+    } else if (knownCivVal == "") {
+      // this means we are using arch to convert civil value
+      double arch = double.parse(knownArchVal);
+      String civil = (arch + double.parse(baseline)).toString();
+      return "Civil Converted Value: " + civil;
+    }
+    return "Invalid inputs, please try again.";
+  } 
+
+  String _getBaseline() {
+    double arch = double.parse(baselineArchVal);
+    double civil = double.parse(baselineCivVal);
+    baseline = (civil - arch).toString();
+    return baseline;
+  }
+
+  bool _checkIfReadyToConvert() {
+    if (knownArchVal == "" && knownCivVal == "") {
+      return false;
+    } else if (knownArchVal != "" && knownCivVal != "") {
+      return false;
+    } else {
+      return true;
+    }
   }
 }
